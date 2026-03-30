78d0de82297c35229a8abcf49d92eeb3e345438e
marco7877 <marcoantonio7877@gmail.com>
2026-03-30 18:13:48 +0200

    fixed directory for ascii

--- main.sh
+++ main.sh
@@ -16,10 +16,10 @@ moon_phase_pct=$(bash ${dir}/moon_phase.sh)
 
 if [ ${moon_phase_pct} -ge 44 ] && [ ${moon_phase_pct} -le 57 ]; then
 	moons=($(ls -d $dir/ascii/*.f))
-	moon=$(cat $dir/ascii/${moons[$index]})
-	echo -e "\035[${moon}\035[m"
+	moon=$(cat ${moons[$index]})
+	echo -e "\035[36m${moon}\035[m"
 
 elif [ ${moon_phase_pct} -ge 94 ] || [ ${moon_phase_pct} -le 7 ]; then
 	moon=$(cat $dir/ascii/*.n)
-	echo -e "\035[${moon}\035[m"
+	echo -e "\033[36m ${moon}\033[m"
 fi
